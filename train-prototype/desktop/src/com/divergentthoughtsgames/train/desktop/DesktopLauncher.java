97b6307df1d6ee7ecb46ae4133db4295d7419f7b
Christopher D. Canfield <cdcanfield@gmail.com>
2014-05-24 01:47:27 -0400

    Added project generated by gradle

---
+++ train-prototype/desktop/src/com/divergentthoughtsgames/train/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.divergentthoughtsgames.train.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.divergentthoughtsgames.train.TrainPrototype;
+
+public class DesktopLauncher
+{
+	public static void main(String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TrainPrototype(), config);
+	}
+}
