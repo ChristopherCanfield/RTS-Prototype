767b4c3b040c0ce82f91114551b440f72f92a018
Christopher D. Canfield <cdcanfield@gmail.com>
2014-06-06 01:27:00 -0400

    Added a second test nav graph

--- train-prototype/core/test/com/divergentthoughtsgames/rts/nav/SearchTest.java
+++ train-prototype/core/test/com/divergentthoughtsgames/rts/nav/SearchTest.java
@@ -102,4 +102,55 @@ public class SearchTest
 		
 		return navGraph;
 	}
+	
+	Node[] createNavGraph2()
+	{
+		Node[] navGraph = new Node[9];
+		navGraph[0] = new Node(0, 0);	// 0
+		navGraph[1] = new Node(0, 1);	// 1
+		navGraph[2] = new Node(0, 2);	// 2
+		navGraph[3] = new Node(1, 0);	// 3
+		navGraph[4] = new Node(1, 1);	// 4
+		navGraph[5] = new Node(1, 2);	// 5
+		navGraph[6] = new Node(2, 0);	// 6
+		navGraph[7] = new Node(2, 1);	// 7
+		navGraph[8] = new Node(2, 2);	// 8
+		
+		Edge edge_00_01 = new Edge(navGraph[0], navGraph[1], 1);
+		Edge edge_00_10 = new Edge(navGraph[0], navGraph[3], 1);
+		Edge edge_00_11 = new Edge(navGraph[0], navGraph[4], 1.4f);
+		navGraph[0].addEdge(edge_00_01).addEdge(edge_00_10).addEdge(edge_00_11);
+		
+		Edge edge_01_02 = new Edge(navGraph[1], navGraph[2], 1);
+		Edge edge_01_11 = new Edge(navGraph[1], navGraph[4], 1);
+		Edge edge_01_10 = new Edge(navGraph[1], navGraph[3], 1.4f);
+		Edge edge_01_12 = new Edge(navGraph[1], navGraph[5], 1.4f);
+		navGraph[1].addEdge(edge_01_02).addEdge(edge_01_10).addEdge(edge_01_11).addEdge(edge_01_12);
+		
+		Edge edge_02_12 = new Edge(navGraph[2], navGraph[5], 1);
+		Edge edge_02_11 = new Edge(navGraph[2], navGraph[4], 1.4f);
+		navGraph[2].addEdge(edge_02_12).addEdge(edge_02_11);
+		
+		Edge edge_10_11 = new Edge(navGraph[3], navGraph[4], 1);
+		Edge edge_10_20 = new Edge(navGraph[3], navGraph[6], 1);
+		Edge edge_10_21 = new Edge(navGraph[3], navGraph[7], 1.4f);
+		navGraph[3].addEdge(edge_10_11).addEdge(edge_10_20).addEdge(edge_10_21);
+		
+		Edge edge_11_12 = new Edge(navGraph[4], navGraph[5], 1);
+		Edge edge_11_21 = new Edge(navGraph[4], navGraph[7], 1);
+		Edge edge_11_20 = new Edge(navGraph[4], navGraph[6], 1.4f);
+		Edge edge_11_22 = new Edge(navGraph[4], navGraph[8], 1.4f);
+		navGraph[4].addEdge(edge_11_12).addEdge(edge_11_21).addEdge(edge_11_20).addEdge(edge_11_22);
+		
+		Edge edge_12_22 = new Edge(navGraph[5], navGraph[8], 1);
+		navGraph[5].addEdge(edge_12_22);
+		
+		Edge edge_20_21 = new Edge(navGraph[6], navGraph[7], 1);
+		navGraph[6].addEdge(edge_20_21);
+		
+		Edge edge_21_22 = new Edge(navGraph[7], navGraph[8], 1);
+		navGraph[7].addEdge(edge_21_22);
+		
+		return navGraph;
+	}
 }
